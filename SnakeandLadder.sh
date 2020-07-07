a47167661ac36f19f5403a6ef110a45ea8b48105
amritha-tk <amrithatk95@gmail.com>
2020-07-07 22:59:55 +0530

    [amritha]Added the Condition to play

--- SnakeandLadder.sh
+++ SnakeandLadder.sh
@@ -1,7 +1,23 @@
-#!/bin/bash -x
+
+#!/bin/bash
 
 echo "Welcome to snake and ladder program"
 START_POSITION=0
 
-diceRoll=$((RANDOM%7))
-echo $diceRoll
+diceRoll=$((RANDOM%6+1))
+echo "DiceRoll no:" $diceRoll
+
+option=$((RANDOM%3))
+if [ $option -eq '0' ]
+    then
+      STARTPOSITION=0
+      echo "NO PLAY  hence Position="$START_POSITION
+ elif [ $option -eq '1' ]
+    then
+       START_POSITION=$(($STARTPOSITION+$diceRoll))
+       echo "LADDER and Position="$START_POSITION
+ else
+      START_POSITION=$(($STARTPOSITION-$diceRoll))
+      echo "Snake and Position="$START_POSITION
+fi
+
