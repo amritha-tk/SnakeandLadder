04ce887e08a9bba144154b2ae36bf5aa76211ac6
amritha-tk <amrithatk95@gmail.com>
2020-07-08 21:33:58 +0530

    [amritha]Added UC7 to find which player won

--- SnakeandLadder.sh
+++ SnakeandLadder.sh
@@ -4,8 +4,13 @@
 
 echo "Welcome to snake and ladder program"
 CURRENT_POSITION=0
-WIN_POSITION=100
+WIN_POSITION=10
 diceRollCount=0
+P1=1
+P2=0
+CURRENT_POSITION_P1=0
+CURRENT_POSITION_P2=0
+PLAY_AGAIN=0
 
 while [ $CURRENT_POSITION -lt $WIN_POSITION ]
         do
@@ -13,19 +18,52 @@ while [ $CURRENT_POSITION -lt $WIN_POSITION ]
         ((diceRollCount++))
         echo "DiceRoll no:" $diceRoll
 
-        option=$((RANDOM%3))
+           if [[ $P1 -eq '1' && $PLAY_AGAIN -eq '0' ]]
+			then
+			echo "Player1"
+			 CURRENT_POSITION=$CURRENT_POSITION_P1
+			 P1=0
+			 P2=1
+			 CURRENT_PLAYER="Player1"
+		elif [[ $P2 -eq '1' && $PLAY_AGAIN -eq '0' ]]
+			then
+			echo "Player2"
+			CURRENT_POSITION=$CURRENT_POSITION_P2
+			P1=1
+			P2=0
+			CURRENT_PLAYER="Player2"
+		elif [[ $P2 -eq '1' && $PLAY_AGAIN -eq '1' ]]
+			then
+			echo "Player 1 again"
+			CURRENT_POSITION=$CURRENT_POSITION_P1
+			P1=0
+			P2=1
+			PLAY_AGAIN=0
+			CURRENT_PLAYER="Player1"
+		else
+			echo "Player 2 again"
+			CURRENT_POSITION=$CURRENT_POSITION_P2
+			P1=1
+			P2=0
+			PLAY_AGAIN=0
+			CURRENT_PLAYER="Player2"
+		fi
+
+		#Conditions for Players
+		option=$((RANDOM%3))
         if [ $option -eq '0' ]
                 then
                    echo "NO PLAY : hence no movement , Position="$CURRENT_POSITION
         elif [ $option -eq '1' ]
                 then
-			CURRENT_POSITION=$(( $CURRENT_POSITION + $diceRoll ))
+                        CURRENT_POSITION=$(( $CURRENT_POSITION + $diceRoll ))
                         if [ $CURRENT_POSITION -gt $WIN_POSITION ]
                                 then
                                         CURRENT_POSITION=$(( $CURRENT_POSITION - $diceRoll ))
                                         echo "LADDER : Position greater than "$WIN_POSITION " hence limiting to position="$CURRENT_POSITION
                         else
                                          echo "LADDER : hence forward movement , Position="$CURRENT_POSITION
+					 PLAY_AGAIN=1
                         fi
         else
                 CURRENT_POSITION=$(( $CURRENT_POSITION - $diceRoll ))
@@ -38,8 +76,15 @@ while [ $CURRENT_POSITION -lt $WIN_POSITION ]
                                 echo "SNAKE : hence reverse movement , Position="$CURRENT_POSITION
                 fi
         fi
+	if [ $CURRENT_PLAYER == "Player1" ]
+		then
+			CURRENT_POSITION_P1=$CURRENT_POSITION
+	else
+			CURRENT_POSITION_P2=$CURRENT_POSITION
+	fi
 done
  echo "Total diceRoll count " $diceRollCount
+echo $CURRENT_PLAYER "is the winner !!!"
 
 
 
