013bc71d8a5aa43e6aed8e7a71e9a7aba8f73814
amritha-tk <amrithatk95@gmail.com>
2020-07-08 14:51:07 +0530

    [amritha]Refactor:Removed some lines

--- SnakeandLadder.sh
+++ SnakeandLadder.sh
@@ -3,10 +3,10 @@
 #!/bin/bash
 
 echo "Welcome to snake and ladder program"
-START_POSITION=0
-WIN_POSITION=100
+CURRENT_POSITION=0
+WIN_POSITION=10
 
-while [ $START_POSITION -lt $WIN_POSITION ]
+while [ $CURRENT_POSITION -lt $WIN_POSITION ]
 	do
 	diceRoll=$((RANDOM%6+1))
 	echo "DiceRoll no:" $diceRoll
@@ -14,26 +14,26 @@ while [ $START_POSITION -lt $WIN_POSITION ]
 	option=$((RANDOM%3))
 	if [ $option -eq '0' ]
 		then
-		   echo "NO PLAY : hence no movement , Position="$START_POSITION
+		   echo "NO PLAY : hence no movement , Position="$CURRENT_POSITION
 	elif [ $option -eq '1' ]
 		then
-		   START_POSITION=$(( $START_POSITION + $diceRoll ))
-			if [ $START_POSITION -gt $WIN_POSITION ]
+		   CURRENT_POSITION=$(( $CURRENT_POSITION + $diceRoll ))
+			if [ $CURRENT_POSITION -gt $WIN_POSITION ]
 				then
-					START_POSITION=$WIN_POSITION
-					echo "LADDER : Position greater than "$WIN_POSITION " hence limiting to position="$START_POSITION
+					CURRENT_POSITION=$WIN_POSITION
+					echo "LADDER : Position greater than "$WIN_POSITION " hence limiting to position="$CURRENT_POSITION
 			else
-					echo "LADDER : hence forward movement , Position="$START_POSITION
+					 echo "LADDER : hence forward movement , Position="$CURRENT_POSITION
 			fi
 	else
-		START_POSITION=$(( $START_POSITION - $diceRoll ))
-		if [ $START_POSITION -lt '0' ]
+		CURRENT_POSITION=$(( $CURRENT_POSITION - $diceRoll ))
+		if [ $CURRENT_POSITION -lt '0' ]
 			then
-				START_POSITION=0
-				echo "SNAKE : Position less than 0 hence restarting to position="$START_POSITION
+				CURRENT_POSITION=0
+				echo "SNAKE : Position less than 0 hence restarting to position="$CURRENT_POSITION
 
 		else
-				echo "SNAKE : hence reverse movement , Position="$START_POSITION
+				echo "SNAKE : hence reverse movement , Position="$CURRENT_POSITION
 		fi
 	fi
 done
